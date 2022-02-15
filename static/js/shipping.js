659706aaf1b38c1dc154ce34b3cdd4d40922d92c
Marcelo <snaker.extreme.15@gmail.com>
2022-02-15 16:20:50 -0300

    [Develop] <Marcelo> Modify shipping js to hide or not some divs and buttons

--- static/js/shipping.js
+++ static/js/shipping.js
@@ -1,4 +1,29 @@
-//var shipping = "{{ order.shipping }}";
 if (shipping == "False") {
   document.getElementById("shipping-info").innerHTML = "";
+}
+
+if (user != "AnonymousUser") {
+  document.getElementById("user-info").innerHTML = "";
+}
+
+if (shipping == "False" && user != "AnonymousUser") {
+  document.getElementById("form-wrapper").classList.add("hidden");
+  document.getElementById("payment-info").classList.remove("hidden");
+}
+
+var form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Form Submited...");
+  document.getElementById("form-button").classList.add("hidden");
+  document.getElementById("payment-info").classList.remove("hidden");
+});
+
+document.getElementById("make-payment").addEventListener("click", function (e) {
+  submitFormData();
+});
+
+function submitFormData() {
+  console.log("Payment button clicked");
 }
